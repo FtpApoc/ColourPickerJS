e88fa82311fb46f68dc8b6a2f5b538d1e63ac653
andrew <test>
2022-12-10 22:28:14 +0000

    work on Box Auto Updates

--- public/js/RgbInput.js
+++ public/js/RgbInput.js
@@ -1,9 +1,33 @@
-const RgbForm = document.getElementById('RGBForm');
+const form = document.getElementById('RgbForm');
 
+console.log("RgbInput.js Connected")
 
-function RgbBoxUpdate(ClickInfo){
-  console.log(ClickInfo)
+function RgbBoxUpdate(){
+  console.log("form has changed");
+  };
+
+console.log(form);
+
+function Test(){
+  const RGBlist = [RgbFormR,RgbFormG,RgbFormB];
+  let FormEntry = false;
+
+  for (let i = 0; i < RGBlist.length; i++){
+    L = RGBlist[i] // L for letter for RGB purposes
+    if ((L.value === "")){ //&& (parseInt(L.value) < 255)){
+      FormEntry = true;
+
+    }
+  }
+
+  if (FormEntry === true){
+    console.log("test")
+  }
+
+  let BoxR = 0
+  let BoxG = 0
+  let BoxB = 0
 };
 
 
-RgbForm.addEventListener('change',RgbBoxUpdate);
+form.addEventListener("change", Test);
