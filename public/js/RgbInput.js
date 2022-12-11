31d001a1fc984e217b6f001a88ab5e32d86cfe08
andrew <test>
2022-12-11 02:23:42 +0000

    Rgb box full functionality

--- public/js/RgbInput.js
+++ public/js/RgbInput.js
@@ -7,27 +7,19 @@ function RgbBoxUpdate(){
   };
 
 console.log(form);
+let PassArray = [0,0,0]
 
-function Test(){
+function PassToBox(){
   const RGBlist = [RgbFormR,RgbFormG,RgbFormB];
   let FormEntry = false;
 
   for (let i = 0; i < RGBlist.length; i++){
     L = RGBlist[i] // L for letter for RGB purposes
-    if ((L.value === "")){ //&& (parseInt(L.value) < 255)){
-      FormEntry = true;
-
+    if ((L.value != "") && (parseInt(L.value) < 255)){
+      PassArray[i] = RGBlist[i].value;
+      }
+      RgbDisplayBox.style.backgroundColor = `rgb(${PassArray[0]},${PassArray[1]},${PassArray[2]})`;
     }
-  }
-
-  if (FormEntry === true){
-    console.log("test")
-  }
-
-  let BoxR = 0
-  let BoxG = 0
-  let BoxB = 0
 };
 
-
-form.addEventListener("change", Test);
+form.addEventListener("change", PassToBox);
